0f760e25cf859acd9daf2f9cde4248005c5e311c
Vytautas K <makrelion@gmail.com>
2023-01-12 09:17:44 +0200

    pridetas class Genre(models.Model)

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField('Pavadinimas', max_length=200, help_text='Iveskite knygos zanra')
+
+    def __str__(self):
+        return self.name
+
