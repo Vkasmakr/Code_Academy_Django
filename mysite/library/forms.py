703bdd0bb7f3baa77a40fb6899150eaa54d91a10
Vytautas K <makrelion@gmail.com>
2023-01-25 13:29:21 +0200

    in pofile page, username change input is now hidden and user cannot change its username anymore

--- mysite/library/forms.py
+++ mysite/library/forms.py
@@ -16,6 +16,7 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+        widgets = {'username': forms.HiddenInput()}
 
 
 class ProfilisUpdateForm(forms.ModelForm):
